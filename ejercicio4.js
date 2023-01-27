a186a51c6eac0551c282e8d829fbeda0491d24ed
Wilson Castillo <dc105677543@gmail.com>
2023-01-27 11:32:01 -0500

    FIRST COMMIT

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,6 +1,6 @@
 let nums=[1,1,0,0];
 let suma=0;
-// salida 0
+// salida 5
 for(let i=0; i<nums.length;i++){
     if(nums[i]==0){
         suma+=i;
--- ejercicio9.js
+++ ejercicio9.js
@@ -1,9 +1,10 @@
 let nums=[0,2,1,4];
 let suma=0;
+// salida 6
 for(let i=0; i<nums.length;i++){
     if(i<nums[i]){
         suma+=i;
     }
 }
-console.log("Suma Indices Menores="+suma);
+console.log("INDICE MAYOR ="+suma);
 
